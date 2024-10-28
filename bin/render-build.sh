3edb804f314dace6b0e645201e2dd2b56c939885
beherit197777 <beherit197777@gmail.com>
2024-10-28 18:38:53 +0300

    fix yarnv5

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,16 +2,14 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
-
-bundle exec rails db:migrate
-bundle exec rails db:seed
-
+bundle config set --local without development
+make install
+make yarn_install
+make make_env_file
+make drop_db
+make db_migrate
+make db_seed
+make compile_assets
+make clear_assets
 
 

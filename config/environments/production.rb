2e27f28e2d5dd7488e69c00ec04cdf451fa8d1c9
beherit197777 <beherit197777@gmail.com>
2024-10-28 15:07:14 +0300

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.hosts << "localhost"
+  config.hosts << "rails-project-65-p78s.onrender.com"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com

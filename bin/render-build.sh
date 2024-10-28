4b934af6842cfec74f917ff5d2e8f922eb66c753
beherit197777 <beherit197777@gmail.com>
2024-10-28 16:22:32 +0300

    fix yarnм2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,14 +2,25 @@
 # exit on error
 set -o errexit
 
+rm -rf node_modules
+rm -rf tmp/cache
+rm -rf public/assets
+
 bundle install
-yarn install
+yarn install --frozen-lockfile
+
 npm install -g esbuild
+
 yarn build
+yarn build:css
+
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
+
+# Проверка прав доступа к файлам
+chmod -R 755 public/assets
 
 
 

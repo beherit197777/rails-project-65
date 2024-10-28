2cb8d2832cb9099e64c6b4915a3faebaec8bf6f4
beherit197777 <beherit197777@gmail.com>
2024-10-28 16:01:33 +0300

    edit render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,20 +2,13 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
 bundle install
-
-# Install JavaScript dependencies
 yarn install
-
-# Build JavaScript assets
 yarn build
-
-# Run database migrations and seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-# Precompile and clean assets
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
 
